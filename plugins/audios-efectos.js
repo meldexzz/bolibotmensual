39a6ddeaa24033727233f54bac7152ebf57a9dfc
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 17:09:39 -0400

    Actualizar audios-efectos.js

--- plugins/audios-efectos.js
+++ plugins/audios-efectos.js
@@ -53,7 +53,7 @@ let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
 handler.help = ['bass', 'blown', 'deep', 'earrape', 'fast', 'fat', 'nightcore', 'reverse', 'robot', 'slow', 'smooth', 'tupai', 'reverb', 'chorus', 'flanger', 'distortion', 'pitch', 'highpass', 'lowpass', 'underwater'].map(v => v + ' *<mp3/vn>*')
 handler.tags = ['audio']
 handler.command = /^(bass|blown|deep|earrape|fas?t|nightcore|reverse|robot|slow|smooth|tupai|squirrel|chipmunk|reverb|chorus|flanger|distortion|pitch|highpass|lowpass|underwater)$/i
-handler.register = true 
+
 export default handler
 
 const getRandom = (ext) => {
