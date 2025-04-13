499f7d9c25b1927bd974598324177e3d7946aa46
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 16:49:44 -0400

    Actualizar stiker-emojimix.js

--- plugins/stiker-emojimix.js
+++ plugins/stiker-emojimix.js
@@ -22,7 +22,5 @@ conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
 handler.help = ['emojimix *<emoji+emoji>*']
 handler.tags = ['sticker']
 handler.command = ['emojimix'] 
-//handler.limit = 1
-handler.register = true 
 
 export default handler
