14043e32a8a00ad924d84eb5be9b0057589c2476
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 17:04:56 -0400

    Actualizar menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -50,5 +50,5 @@ global.db.data.users[m.sender].lastcofre = new Date * 1
 handler.help = ['menuff']
 handler.tags = ['freefire' ,'main'] 
 handler.command = ['menuff', 'menufreefire'] 
-handler.register = true
+
 export default handler
