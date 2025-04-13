013e58652060db1cd5334c58ec671b7667b00dab
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 16:56:20 -0400

    Actualizar nsfw-r34.js

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -33,6 +33,5 @@ await m.reply(`🚩 Ocurrió un error. ${error.message}`);
 handler.help = ['r34 <texto>'];
 handler.command = ['r34', 'rule34'];
 handler.tags = ['nsfw'];
-handler.register = true;
 // handler.estrellas = 2;
 export default handler;
