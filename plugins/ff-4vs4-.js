18f7690314fa0d88024fd69003dcb18356d3c775
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 11:13:05 -0400

    Actualizar ff-4vs4-.js

--- plugins/ff-4vs4-.js
+++ plugins/ff-4vs4-.js
@@ -7,72 +7,44 @@ let anotados = []; // Lista de usuarios anotados
 let handler = async (m, { conn, args, command, usedPrefix }) => {
     // Mostrar la lista inicial de anotados
     if (command === '4vs4' && !args[0]) {
-        let lista = `
-𝟒 𝐕𝐄𝐑𝐒𝐔𝐒 𝟒
-
-⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎                       
-🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎 : 
-🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀 :                
-
-➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃: 
-➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
-
-𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
-👑 ┇ ${anotados.length > 0 ? anotados.join('\n👑 ┇ ') : 'Vacío'}
-    
-ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
-🥷🏻 ┇ Vacío
-`;
+        let lista = `𝟒 𝐕𝐄𝐑𝐒𝐔𝐒 𝟒\n\n⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎\n🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎\n🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀\n\n➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃\n➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:\n\n𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1\n👑 ┇ ${anotados.length > 0 ? anotados.join('\n👑 ┇ ') : 'Vacío'}\n\nㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n🥷🏻 ┇ Vacío`;
 
         const buttons = [
             { buttonId: `${usedPrefix}anotarse`, buttonText: { displayText: "✍️ Anotarse" }, type: 1 }
         ];
 
-        await conn.sendMessage(m.chat, {
+        const buttonMessage = {
             text: lista,
             footer: "Presiona el botón para anotarte",
             buttons: buttons,
             headerType: 1
-        });
+        };
 
+        await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
         return;
     }
 
     // Registrar al usuario al presionar el botón de anotarse
     if (command === 'anotarse') {
         if (anotados.includes(m.sender)) {
-            await conn.sendMessage(m.chat, { text: "❗ Ya estás anotado." });
+            await conn.sendMessage(m.chat, { text: "❗ Ya estás anotado." }, { quoted: m });
         } else {
             anotados.push(m.sender); // Agregar al usuario a la lista
-            let listaActualizada = `
-𝟒 𝐕𝐄𝐑𝐒𝐔𝐒 𝟒
-
-⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎                       
-🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎 : 
-🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀 :                
-
-➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃: 
-➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
-
-𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
-👑 ┇ ${anotados.join('\n👑 ┇ ')}
-    
-ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
-🥷🏻 ┇ Vacío
-`;
+            let listaActualizada = `𝟒 𝐕𝐄𝐑𝐒𝐔𝐒 𝟒\n\n⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎\n🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎\n🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀\n\n➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃\n➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:\n\n𝗘𝗦𝗖𝗨𝗔𝐃𝗥𝗔 1\n👑 ┇ ${anotados.join('\n👑 ┇ ')}\n\nㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n🥷🏻 ┇ Vacío`;
 
             const buttons = [
                 { buttonId: `${usedPrefix}anotarse`, buttonText: { displayText: "✍️ Anotarse" }, type: 1 }
             ];
 
-            await conn.sendMessage(m.chat, {
+            const buttonMessage = {
                 text: listaActualizada,
                 footer: "Presiona el botón para anotarte",
                 buttons: buttons,
                 headerType: 1
-            });
-        }
+            };
 
+            await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
+        }
         return;
     }
 };
