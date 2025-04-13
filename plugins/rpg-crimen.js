bace53d94d823d088ead503d07606dfcbe61bb47
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 16:49:09 -0400

    Actualizar rpg-crimen.js

--- plugins/rpg-crimen.js
+++ plugins/rpg-crimen.js
@@ -60,7 +60,6 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 handler.tags = ["rpg"];
 handler.help = ["crimen"];
 handler.command = ["crimen", "crime"];
-handler.register = true;
 handler.group = true;
 
 export default handler;
