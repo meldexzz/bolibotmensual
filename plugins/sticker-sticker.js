9020009de635f031795eb93a887c8b1ae879fb00
Wichi <sebastianbarbaro82@gmail.com>
2025-04-12 22:54:48 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -59,7 +59,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
             externalAdReply: { 
               showAdAttribution: false, 
               title: global.packname, 
-              body: `botbarboza - Ai ☃️`, 
+              body: `botBolillo - Ai ☃️`, 
               mediaType: 2, 
               sourceUrl: redes, // Usamos la variable definida
               thumbnail: icons // Asegúrate de que "icons" tenga un valor definido
