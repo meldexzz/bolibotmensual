023341544e4b13dc4d27b65fb9efdec2c8f5683c
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 17:11:58 -0400

    Actualizar rpg-inventario.js

--- plugins/rpg-inventario.js
+++ plugins/rpg-inventario.js
@@ -42,6 +42,6 @@ let handler = async (m, { conn, usedPrefix }) => {
 handler.help = ['inventario', 'inv'];
 handler.tags = ['rpg'];
 handler.command = ['inventario', 'inv']; 
-handler.register = true;
+
 
 export default handler;
