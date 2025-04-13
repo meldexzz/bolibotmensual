a016d923376ef46c6a0532ca69a662f1fa8f883f
Wichi <sebastianbarbaro82@gmail.com>
2025-04-12 22:54:02 -0400

    Actualizar group-crear.js

--- plugins/group-crear.js
+++ plugins/group-crear.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
   // Notificar al usuario que se está generando la imagen
   await conn.sendMessage(m.chat, {
-    text: `🔨 *BarbozaBot-Ai está creando tu imagen... Por favor espera...*`,
+    text: `🔨 *creando tu imagen... Por favor espera...*`,
   });
 
   try {
@@ -29,7 +29,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     // Enviar la imagen al usuario
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `🖼️ *Aquí está tu imagen generada por BarbozaBot-Ai:*`,
+      caption: `🖼️ *Aquí está tu imagen generada:*`,
     });
   } catch (error) {
     console.error("Error al generar la imagen:", error);
