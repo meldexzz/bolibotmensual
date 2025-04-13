a4dcac287a8144ec94e31961c54cbd8b48854962
Wichi <sebastianbarbaro82@gmail.com>
2025-04-12 22:28:34 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,116 +1,58 @@
-import axios from 'axios'
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import moment from 'moment-timezone'
+import { createHash } from 'crypto';
 
-let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
-let handler = async function (m, { conn, text, args, usedPrefix, command }) {
-    let user = global.db.data.users[m.sender]
-    let name2 = conn.getName(m.sender)
-    let whe = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-    let perfil = await conn.profilePictureUrl(whe, 'image').catch(_ => 'https://qu.ax/Mvhfa.jpg')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i;
+let handler = async function (m, { conn, text, usedPrefix, command }) {
+    let user = global.db.data.users[m.sender];
+    let name2 = conn.getName(m.sender);
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
+    let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/1fx3bv01/file.jpg');
 
-    if (user.registered === true) {
-        return m.reply(`💛 𝗬𝗮 𝘁𝗲 𝗲𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮𝘀 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗱𝗼.\n\n¿𝗤𝘂𝗶𝗲𝗿𝗲 𝘃𝗼𝗹𝘃𝗲𝗿 𝗮 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗿𝘀𝗲?\n\n𝗨𝘀𝗲 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼 𝗽𝗮𝗿𝗮 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿 𝘀𝘂 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗼.\n*${usedPrefix}unreg*`)
-    }
+    if (user.registered === true) throw `*⚠️ Ya estás registrado*\n\n¿Quieres volver a registrarte?\n\n💬 Usa este comando para *eliminar tu registro*:\n*${usedPrefix}unreg* <Número de serie>`;
+    if (!Reg.test(text)) throw `*⚠️ Formato incorrecto*\n\n📝 Uso del comando: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18`;
 
-    if (!Reg.test(text)) return m.reply(`Eʟ ғᴏʀᴍᴀᴛᴏ ɪɴɢʀᴇsᴀᴅᴏ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴏ\n\nUsᴏ ᴅᴇʟ ᴄᴏᴍᴀɴᴅᴏ: ${usedPrefix + command} 𝗻𝗼𝗺𝗯𝗿𝗲.𝗲𝗱𝗮𝗱\nEᴊᴇᴍᴘʟᴏ : *${usedPrefix + command} ${name2}.14*`)
+    let [_, name, splitter, age] = text.match(Reg);
+    if (!name) throw '*📝 El nombre no puede estar vacío*';
+    if (!age) throw '*📝 La edad no puede estar vacía*';
+    if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*'; 
+    age = parseInt(age);
+    if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar con el bot*';
+    if (age < 5) throw '*👀 Hay un bebé jsjsj*';
 
-    let [_, name, splitter, age] = text.match(Reg)
-    if (!name) return m.reply('💛 Eʟ ɴᴏʍ𝗯𝗿𝗲 ɴᴏ ᴘᴜᴇᴅᴇ ᴇsᴛᴀʀ ᴠᴀᴄɪᴏ.')
-    if (!age) return m.reply('💛 Lᴀ ᴇᴅᴀᴅ ɴᴏ ᴘᴜᴇᴅᴇ ᴇsᴛᴀʀ ᴠᴀᴄɪ́ᴀ.')
-    if (name.length >= 100) return m.reply('💛 El nombre es demasiado largo.')
+    user.name = name.trim();
+    user.age = age;
+    user.regTime = + new Date();
+    user.registered = true;
 
-    age = parseInt(age)
-    if (age > 1000) return m.reply('*ʟᴀ ᴇᴅᴀᴅ ɪɴɢʀᴇsᴀᴅᴀ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴀ*')
-    if (age < 5) return m.reply('*ʟᴀ ᴇᴅᴀᴅ ɪɴɢʀᴇsᴀᴅᴀ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴀ*')
+    if (!user.limit) user.limit = 0;
+    user.limit += 10;
 
-    user.name = name.trim()
-    user.age = age
-    user.regTime = +new Date
-    user.registered = true
-    global.db.data.users[m.sender].money += 600
-    global.db.data.users[m.sender].estrellas += 10
-    global.db.data.users[m.sender].exp += 245
-    global.db.data.users[m.sender].joincount += 5    
+    let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6);
+    m.react('📩');
 
-    let who;
-    if (m.quoted && m.quoted.sender) {
-        who = m.quoted.sender;
-    } else {
-        who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    }
+    let regbot = `🗃️ *R E G I S T R A D O* 🗃️\n
+💌 *Nombre:* ${name}
+📆 *Edad* : ${age} años
+🍬 *Dulces añadidos:* 10`;
 
-    let sn = createHash('md5').update(m.sender).digest('hex')
-    let regbot = `┏━━━━━━━━━━━━━━━━━━⬣
-┃⋄ *🎩 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 - BARBOZA𝐁𝐎𝐓*
-┗━━━━━━━━━━━━━━━━━━⬣\n`
-    regbot += `•✩.･*:｡≻──── ⋆♡⋆ ────.•*:｡✩•\n`
-    regbot += `*「💛」𝐍𝐨𝐦𝐛𝐫𝐞:* ${name}\n`
-    regbot += `*「💛」𝐄𝐝𝐚𝐝:* ${age} años\n`
-    regbot += `•✩.･*:｡≻──── ⋆♡⋆ ────.•*:｡✩•\n`
-    regbot += `*「🎀」𝐑𝐞𝐜𝐨𝐦𝐩𝐞𝐧𝐬𝐚𝐬:*\n> `
-    regbot += `• 15 Estrellas 🌟\n> `
-    regbot += `• 5 Coins 🪙\n> `
-    regbot += `• 245 Experiencia 💸\n> `
-    regbot += `• 12 Tokens 💰\n`
-    regbot += `꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷\n> `
-    regbot += `🍫 Usᥲ *#perfil* ⍴ᥲrᥲ ᥎ᥱr 𝗍ᥙ ⍴ᥱr𝖿іᥣ.`
-
-  await conn.sendMessage(m.chat, {
-        text: regbot,
-        contextInfo: {
-            externalAdReply: {
-                title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
-                thumbnailUrl: 'https://qu.ax/Mvhfa.jpg',
-                mediaType: 1,
-                renderLargerThumbnail: true
-            }
+    // Crear botón de "Menú"
+    const buttons = [
+        {
+            buttonId: `${usedPrefix}menu`,
+            buttonText: { displayText: "📜 Menú" },
+            type: 1
         }
+    ];
+
+    await conn.sendMessage(m.chat, { 
+        image: { url: pp }, 
+        caption: regbot,
+        buttons: buttons,
+        viewOnce: true
     }, { quoted: m });
-
-/*    await m.react('📪')
-  await conn.sendMessage(m.chat, {
-           text: regbot, 
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: true,                      
-                containsAutoReply: true,     
-                renderLargerThumbnail": true,
-                title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',  
-                body: dev,  
-                containsAutoReply: true,
-                showAdAttribution: true,
-                mediaType: 1, 
-                thumbnailUrl: 'https://qu.ax/Mvhfa.jpg' }}}, {quoted: m})
-*/
-
-let chtxt = `👤 *𝚄𝚜𝚎𝚛* » ${m.pushName || 'Anónimo'}
-🗂 *𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊𝚌𝚒𝚘́𝚗* » ${user.name}
-🍨 *𝙴𝚍𝚊𝚍* » ${user.age} años
-⌨️ *𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚌𝚒𝚘𝚗* » ${user.descripcion}
-🍭 *𝙽𝚞𝚖𝚎𝚛𝚘 𝚍𝚎 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚘* »
-⤷ ${sn}`;
-
-    let channelID = '120363414007802886@newsletter';
-        await conn.sendMessage(channelID, {
-        text: chtxt,
-        contextInfo: {
-            externalAdReply: {
-                title: "【 🔔 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐂𝐈𝐎́𝐍 𝐃𝐄 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 🔔 】",
-                body: '🥳 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚗𝚞𝚎𝚟𝚘 𝚎𝚗 𝚖𝚒 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜!',
-                thumbnailUrl: 'perfil',
-                sourceUrl: 'redes',
-                mediaType: 1,
-                showAdAttribution: false,
-                renderLargerThumbnail: false
-            }
-        }
-    }, { quoted: null });
 };
 
-handler.help = ['reg']
-handler.tags = ['rg']
-handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar']
+handler.help = ['reg'];
+handler.tags = ['rg'];
+handler.command = ['verify', 'reg', 'verificar'];
 
-export default handler
+export default handler;
