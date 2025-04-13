074e63381ac97ecdd64260306cb14feb1860e6f1
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 17:10:25 -0400

    Actualizar gamer-casino.js

--- plugins/gamer-casino.js
+++ plugins/gamer-casino.js
@@ -45,7 +45,6 @@ let handler = async (m, { conn, args, usedPrefix, command, DevMode }) => {
 handler.help = ['apostar *<cantidad>*']
 handler.tags = ['game']
 handler.command = /^(apostar|casino)$/i
-handler.register = true
 
 handler.fail = null
 
