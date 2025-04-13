597d051648188d2c709a4e59cfed1b3917f21970
Wichi <sebastianbarbaro82@gmail.com>
2025-04-13 17:11:39 -0400

    Actualizar descargas-capcut.js

--- plugins/descargas-capcut.js
+++ plugins/descargas-capcut.js
@@ -30,6 +30,5 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['capcutdownload *<url cc>*'];
 handler.tags = ['downloader'];
 handler.command = ['capcut', 'ccdownload'];
-handler.register = true;
 
 export default handler;
