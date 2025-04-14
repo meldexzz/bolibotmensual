3238fc98b1d693d7cf6e826564010d5b1f3d0403
Wichi <sebastianbarbaro82@gmail.com>
2025-04-14 15:14:38 -0400

    Actualizar group-config.js

--- plugins/group-config.js
+++ plugins/group-config.js
@@ -1,7 +1,7 @@
 
 var handler = async (m, {conn, args, usedPrefix, command}) => {
 const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
-if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, rcanal, )}
+if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m )}
 await conn.groupSettingUpdate(m.chat, isClose)
 { 
 conn.reply(m.chat, '✅ *Configurado correctamente*', m )
