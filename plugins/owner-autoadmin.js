a1976f4b412c066b6d5a972a2d8a643774c7c03d
Wichi <sebastianbarbaro82@gmail.com>
2025-04-14 15:11:03 -0400

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   await m.react(done)
    m.reply('🚩 *¡YA TE DI ADM MI JEFE!*');
     let nn = conn.getName(m.sender);
-     conn.reply('544123989549@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('544123989549@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m );
   } catch {
     m.reply('Demasiado Bueno 👻');
   }
