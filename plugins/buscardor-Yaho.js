fa946b3f94d12e27e298e64a68b45417eb77fafc
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-15 17:15:03 -0600

    Update buscardor-Yaho.js

--- plugins/buscardor-Yaho.js
+++ plugins/buscardor-Yaho.js
@@ -5,27 +5,27 @@ import fetch from 'node-fetch';
 
 let MF = async(m, { conn, args }) => {
 
-if (!args[0]) return conn.reply(m.chat, '🌙 INGRESE UN TEXTO PARA BUSCAR EN YAHOO', m);
+if (!args[0]) return conn.reply(m.chat, '> 𝘐𝘯𝘨𝘳𝘦𝘴𝘦 𝘶𝘯 𝘵𝘦𝘹𝘵𝘰 𝘱𝘢𝘳𝘢 𝘣𝘶𝘴𝘤𝘢𝘳 𝘦𝘯 𝘺𝘢𝘩𝘰𝘰!.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘺𝘢𝘩𝘰𝘰𝘴𝘦𝘢𝘳𝘤𝘩 𝘣𝘰𝘭𝘪𝘭𝘭𝘰𝘴.🥖', m);
 
 try {
 let api = await (await fetch(`https://archive-ui.tanakadomp.biz.id/search/yahoosearch?q=${args[0]}`)).json();
 
-let moon = `\`𝚈𝙰𝙷𝙾𝙾 𝑋 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰\`.`
+let moon = `> 𝘠𝘈𝘏𝘖𝘖! / 𝘉𝘖𝘓𝘐𝘓𝘓𝘖𝘉𝘖𝘛.🥖.`
 for (let i = 0; i < (5 <= api.result.length ? 5 : api.result.length); i++) {
 
 let force = api.result[i];
 
 moon += `\n\n`
-moon += `☪︎ *Título:* ${force.title}\n`
-moon += `☪︎ *Enlace:* ${force.link}\n\n`
-moon += `☪︎ *Descripción:* ${force.snippet}\n`
-moon += `───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ─────`
+moon += `> 𝘛𝘪𝘵𝘶𝘭𝘰: ${force.title}\n`
+moon += `> 𝘌𝘯𝘭𝘢𝘤𝘦: ${force.link}\n\n`
+moon += `> 𝘌𝘯𝘭𝘢𝘤𝘦: ${force.snippet}\n`
+moon += ``
 }
 
 conn.sendMessage(m.chat, { text: moon }, { quoted: m });
 
 } catch (e) {
-m.reply(`*Error En La Api*`);
+m.reply(`𝘌𝘳𝘳𝘰𝘳 𝘦𝘯 𝘭𝘢 𝘈𝘗𝘐.🥖`);
 m.react('✖️');
 }}
 
