03c359a5bb027bef7086b29a7ba005c874b64bf8
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-15 13:32:33 -0600

    Update _tiktokvalle.js

--- plugins/_tiktokvalle.js
+++ plugins/_tiktokvalle.js
@@ -2,7 +2,7 @@
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let res = await tiktokvalle[Math.floor(Math.random() * tiktokvalle.length)]
 await m.react('💔')
-conn.sendMessage(m.chat, { video: { url: res }, caption: `» 𝗩𝗔𝗟𝗟𝗘𝗡𝗔𝗧𝗢𝗦 𝗧𝗜𝗞𝗧𝗢𝗞 💔` }, { quoted: m })}
+conn.sendMessage(m.chat, { video: { url: res }, caption: `> 𝘝𝘢𝘭𝘭𝘦𝘯𝘢𝘵𝘰𝘴.🥖` }, { quoted: m })}
 handler.help = ['tiktokvalle']
 handler.tags = ['random']
 handler.command = /^(tiktokvalle)$/i
