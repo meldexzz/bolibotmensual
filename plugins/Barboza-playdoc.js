b5cada6797b4648f2324185b27aa173194235943
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-14 22:34:03 -0600

    Update Barboza-playdoc.js

--- plugins/Barboza-playdoc.js
+++ plugins/Barboza-playdoc.js
@@ -4,14 +4,14 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return m.reply(`> _𝘐𝘯𝘨𝘳𝘦𝘴𝘢 𝘶𝘯 𝘵𝘦𝘹𝘵𝘰 𝘱𝘢𝘳𝘢 𝘣𝘶𝘴𝘤𝘢𝘳 𝘦𝘯 𝘠𝘰𝘶𝘛𝘶𝘣𝘦.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: ${usedPrefix + command} 𝘣𝘰𝘭𝘪𝘭𝘭𝘰𝘴 𝘣𝘦𝘣𝘦𝘴_`);
+if (!text) return m.reply(`> 𝘐𝘯𝘨𝘳𝘦𝘴𝘢 𝘶𝘯 𝘵𝘦𝘹𝘵𝘰 𝘱𝘢𝘳𝘢 𝘣𝘶𝘴𝘤𝘢𝘳 𝘦𝘯 𝘠𝘰𝘶𝘛𝘶𝘣𝘦.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘱𝘭𝘢𝘺𝘥𝘰𝘤 𝘣𝘰𝘭𝘪𝘭𝘭𝘰𝘴 𝘣𝘦𝘣𝘦𝘴. 🥖 `);
 
 try {
 let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
 
 let results = api.data[0];
 
-let txt = `✨ *Título:* ${results.title}\n⌛ *Duración:* ${results.duration}\n📎 *Link:* ${results.url}\n📆 *Publicado:* ${results.publishedAt}`;
+let txt = `> 𝘛𝘪𝘵𝘶𝘭𝘰: ${results.title}\n> 𝘋𝘶𝘳𝘢𝘤𝘪𝘰𝘯: ${results.duration}\n> 𝘓𝘪𝘯𝘬: ${results.url}\n> 𝘗𝘶𝘣𝘭𝘪𝘤𝘢𝘥𝘰: ${results.publishedAt}`;
 
 let img = results.image;
 
