de422e1a59260ea2e0e81b690fc87b8b4eabb89f
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-15 13:06:53 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -13,7 +13,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `🚩 No permitimos enlaces de otros grupos, lo siento *@${m.sender.split('@')[0]}* serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
+        await conn.reply(m.chat, `> 𝘕𝘰 𝘱𝘦𝘳𝘮𝘪𝘵𝘪𝘮𝘰𝘴 𝘦𝘯𝘭𝘢𝘤𝘦𝘴 𝘥𝘦 𝘰𝘵𝘳𝘰 𝘨𝘳𝘶𝘱𝘰 *_@${m.sender.split('@')[0]}_* 𝘴𝘦𝘳á𝘴 𝘦𝘹𝘱𝘶𝘭𝘴𝘢𝘥𝘰 𝘥𝘦𝘭 𝘨𝘳𝘶𝘱𝘰.🥖  ${isBotAdmin ? '' : '\n\n𝘕𝘰 𝘴𝘰𝘺 𝘢𝘥𝘮𝘪𝘯𝘪𝘴𝘵𝘳𝘢𝘥𝘰𝘳 𝘥𝘦𝘭 𝘨𝘳𝘶𝘱𝘰, 𝘯𝘰 𝘱𝘶𝘦𝘥𝘰 𝘦𝘹𝘱𝘶𝘭𝘴𝘢𝘳𝘵𝘦.'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
                 await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
