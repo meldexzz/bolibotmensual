3451aa6120c15f21c0e2e810b2e105b08c7d37e5
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-15 17:35:08 -0600

    Update d.js

--- plugins/d.js
+++ plugins/d.js
@@ -3,10 +3,10 @@ import path from 'path';
 
 var handler = async (m, { conn, usedPrefix }) => {
     if (global.conn.user.jid !== conn.user.jid) {
-        return conn.reply(m.chat, '☯︎ *Utiliza este comando directamente en el número principal del Bot*', m);
+        return conn.reply(m.chat, '> 𝘜𝘵𝘪𝘭𝘪𝘻𝘢 𝘦𝘴𝘵𝘦 𝘤𝘰𝘮𝘢𝘯𝘥𝘰 𝘥𝘪𝘳𝘦𝘤𝘵𝘢𝘮𝘦𝘯𝘵𝘦 𝘦𝘯 𝘦𝘭 𝘯𝘶𝘮𝘦𝘳𝘰 𝘥𝘦𝘭 𝘉𝘰𝘵..🥖', m);
     }
 
-    await conn.reply(m.chat, '🂱 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m);
+    await conn.reply(m.chat, '> 𝘐𝘯𝘪𝘤𝘪𝘢𝘯𝘥𝘰 𝘱𝘳𝘰𝘤𝘦𝘴𝘰 𝘥𝘦 𝘦𝘭𝘪𝘮𝘪𝘯𝘢𝘤𝘪ó𝘯 𝘥𝘦 𝘢𝘳𝘤𝘩𝘪𝘷𝘰𝘴 𝘥𝘦 𝘴𝘦𝘴𝘪ó𝘯..🥖', m);
 
     // Define rwait como un emoji o identificador
     const rwait = '⏳'; // Emoji de espera
@@ -15,7 +15,7 @@ var handler = async (m, { conn, usedPrefix }) => {
     let sessionPath = './seccion-activas';
     try {
         if (!existsSync(sessionPath)) {
-            return await conn.reply(m.chat, '💻 *La carpeta ya fue limpiada*', m);
+            return await conn.reply(m.chat, '> ¿𝘠𝘢 𝘮𝘦 𝘷𝘦𝘴 𝘢𝘩𝘰𝘳𝘢, 𝘉𝘰𝘭𝘪𝘭𝘭𝘰?.🥖', m);
         }
 
         let files = await fs.readdir(sessionPath);
@@ -29,16 +29,16 @@ var handler = async (m, { conn, usedPrefix }) => {
         }
 
         if (filesDeleted === 0) {
-            await conn.reply(m.chat, '💻 *La carpeta ya fue limpiada*', m);
+            await conn.reply(m.chat, '> ¿𝘠𝘢 𝘮𝘦 𝘷𝘦𝘴 𝘢𝘩𝘰𝘳𝘢, 𝘉𝘰𝘭𝘪𝘭𝘭𝘰?.🥖', m);
         } else {
             const done = '✅'; // Emoji de hecho
             m.react(done);
-            await conn.reply(m.chat, `⚠︎ *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`, m);
-            await conn.reply(m.chat, '𒊹︎︎︎ *¿Me ves o no futuro cliente?*', m);
+            await conn.reply(m.chat, `> 𝘐𝘯𝘪𝘤𝘪𝘢𝘯𝘥𝘰 𝘱𝘳𝘰𝘤𝘦𝘴𝘰 𝘥𝘦 𝘦𝘭𝘪𝘮𝘪𝘯𝘢𝘤𝘪ó𝘯 𝘥𝘦 𝘢𝘳𝘤𝘩𝘪𝘷𝘰𝘴 𝘥𝘦 𝘴𝘦𝘴𝘪ó𝘯..🥖`, m);
+            await conn.reply(m.chat, '> ¿𝘠𝘢 𝘮𝘦 𝘷𝘦𝘴 𝘢𝘩𝘰𝘳𝘢, 𝘉𝘰𝘭𝘪𝘭𝘭𝘰?.🥖', m);
         }
     } catch (err) {
-        console.error('Error al leer la carpeta o los archivos de sesión:', err);
-        await conn.reply(m.chat, '𖠌 *Ocurrió un fallo*', m);
+        console.error('> 𝘐𝘯𝘪𝘤𝘪𝘢𝘯𝘥𝘰 𝘱𝘳𝘰𝘤𝘦𝘴𝘰 𝘥𝘦 𝘦𝘭𝘪𝘮𝘪𝘯𝘢𝘤𝘪ó𝘯 𝘥𝘦 𝘢𝘳𝘤𝘩𝘪𝘷𝘰𝘴 𝘥𝘦 𝘴𝘦𝘴𝘪ó𝘯..🥖', err);
+        await conn.reply(m.chat, '> ¿𝘠𝘢 𝘮𝘦 𝘷𝘦𝘴 𝘢𝘩𝘰𝘳𝘢, 𝘉𝘰𝘭𝘪𝘭𝘭𝘰?.🥖', m);
     }
 }
 
