548f9e95318af51451cc608936fb82a38ab8583c
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-14 23:10:46 -0600

    Update Pussy.js

--- plugins/Pussy.js
+++ plugins/Pussy.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   try {
    
     if (command !== Buffer.from("cHVzc3k=", "base64").toString("utf-8")) {
-      throw new Error("Comando no reconocido.");
+      throw new Error("𝘊𝘰𝘮𝘢𝘯𝘥𝘰 𝘯𝘰 𝘳𝘦𝘤𝘰𝘯𝘰𝘤𝘪𝘥𝘰.🥖");
     }
 
    
@@ -14,7 +14,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     const response = await fetch(apiUrl);
 
     if (!response.ok) {
-      throw new Error(`La API devolvió un estado HTTP ${response.status}.`);
+      throw new Error(`𝘓𝘢 𝘈𝘗𝘐 𝘥𝘦𝘷𝘰𝘭𝘷𝘪𝘰 𝘶𝘯 𝘦𝘴𝘵𝘢𝘥𝘰 𝘏𝘛𝘛𝘗 ${response.status}.🥖`);
     }
 
     const imageBuffer = await response.buffer();
@@ -24,10 +24,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
       caption: Buffer.from("8J+PjSBBCcOkcO8YSB0aWVuZXMgdW5hIGltYWdlbiBhbGVhdG9yaWEgc29saWNpdGFkYSBjb24gZWwgY29tYW5kbyAq", "base64").toString("utf-8") + `${usedPrefix}${command}*.`,
     });
   } catch (error) {
-    console.error("Error al obtener la imagen:", error);
+    console.error("𝘌𝘳𝘳𝘰𝘳 𝘥𝘦𝘴𝘤𝘰𝘯𝘰𝘤𝘪𝘥𝘰. 🥖", error);
     await conn.sendMessage(m.chat, {
       text: Buffer.from("4piNICBPY3VycmlvIHVuIGVycm9yIGFsIGludGVudGFyIHByb2Nlc2FyIHR1IHNvbGljaXR1ZDo=", "base64").toString("utf-8") +
-        `\n${error.message || "Error desconocido"}`,
+        `\n${error.message || "𝘌𝘳𝘳𝘰𝘳 𝘥𝘦𝘴𝘤𝘰𝘯𝘰𝘤𝘪𝘥𝘰. 🥖 "}`,
     });
   }
 };
