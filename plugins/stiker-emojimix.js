21e0d18a6307b97572e66787a2b0c96ef4002b73
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-16 10:27:59 -0600

    Update stiker-emojimix.js

--- plugins/stiker-emojimix.js
+++ plugins/stiker-emojimix.js
@@ -12,7 +12,7 @@ resolve(json)
 reject(err)
 })})
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply(`✨ Ejemplo: *${usedPrefix + command}* 😎+🤑`)
+if (!args[0]) return m.reply(`> ¿𝘘𝘶𝘪𝘦𝘳𝘦𝘴 𝘤𝘳𝘦𝘢𝘳 𝘶𝘯 𝘦𝘮𝘰𝘫𝘪, 𝘢 𝘣𝘢𝘴𝘦 𝘥𝘦 𝘥𝘰𝘴?. 𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘦𝘮𝘰𝘫𝘪𝘮𝘪𝘹 😀 + 😉.`)
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
