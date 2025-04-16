f58722aac3859e868a3ea4abfdfd1ccc4c39cc98
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-16 09:54:59 -0600

    Update toosl-tomp3.js

--- plugins/toosl-tomp3.js
+++ plugins/toosl-tomp3.js
@@ -3,7 +3,7 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `✅ Responde al *Video* o *Nota de Voz* que desea convertir a mp3.`
+if (!/video|audio/.test(mime)) throw `> 𝘙𝘦𝘴𝘱𝘰𝘯𝘥𝘦 𝘢𝘭 "𝘝𝘪𝘥𝘦𝘰" 𝘰 "𝘕𝘰𝘵𝘢 𝘥𝘦 𝘝𝘰𝘻" 𝘲𝘶𝘦 𝘥𝘦𝘴𝘦𝘢 𝘤𝘰𝘯𝘷𝘦𝘳𝘵𝘪𝘳 𝘢 𝘮𝘱3.🥖`
 try {
 let media = await q.download?.()
 if (!media) return null
