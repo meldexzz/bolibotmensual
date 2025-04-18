b3b2f372dfc344b10242c6f738e689227dbe3a44
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-04-18 16:28:27 -0600

    Update search-spotify.js

--- plugins/search-spotify.js
+++ plugins/search-spotify.js
@@ -11,18 +11,15 @@ import fetch from 'node-fetch';
 
 let MF = async (m, { conn, args, command, usedPrefix }) => {
 
-if (!args[0]) return m.reply(`🌙 INGRESE UN Link De Spotify\n> *Ejemplo:* ${usedPrefix + command} https://open.spotify.com/track/0jH15Y9z2EpwTWRQI11xbj`);
+if (!args[0]) return m.reply(`> 𝘐𝘯𝘨𝘳𝘦𝘴𝘦 𝘶𝘯 𝘭𝘪𝘯𝘬 𝘥𝘦 𝘚𝘱𝘰𝘵𝘪𝘧𝘺.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘴𝘱𝘰𝘵𝘪𝘧𝘺𝘥𝘭 𝘩𝘵𝘵𝘱𝘴://𝘰𝘱𝘦𝘯.𝘴𝘱𝘰𝘵𝘪𝘧𝘺.𝘤𝘰𝘮/𝘵𝘳𝘢𝘤𝘬/0𝘫𝘏15𝘠9𝘻2𝘌𝘱𝘸𝘛𝘞𝘙𝘘𝘐11𝘹𝘣𝘫.🥖`);
 
 let api = await (await fetch(`https://archive-ui.tanakadomp.biz.id/download/spotify?url=${args[0]}`)).json();
 
 let force = api.result.data;
 let imagen = force.image;
 
-let moon = `\`𝚂𝙿𝙾𝚃𝙸𝙵𝚈 𝑋 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰\`.\n\n`
-moon += `☪︎ *Título:* ${force.title}\n`
-moon += `☪︎ *Artista:* ${force.artis}\n`
-moon += `☪︎ *Duración:* ${force.durasi}\n`
-moon += `───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ─────`;
+let moon = `> 𝘗𝘰𝘸𝘦𝘳 𝘉𝘺 𝘉𝘰𝘭𝘪𝘭𝘭𝘰𝘉𝘰𝘵.🥖`
+moon += `> 𝘛𝘪𝘵𝘶𝘭𝘰: ${force.title}\n`;
 
 conn.sendFile(m.chat, imagen, 'MoonForce.jpg', moon, m, null);
 
